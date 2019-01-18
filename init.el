1ff647730199ebe68fbc177aed8e356ead2a921b
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-18 15:15:04 +0100

    initial commit: adds basic configuration and package manager setup

---
+++ init.el
@@ -0,0 +1,45 @@
+;; UI
+;;; deactivate ui chrome
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+
+;;; typography
+(add-to-list 'default-frame-alist '(font . "Cartograph Mono CF-14"))
+(setq-default line-spacing 20)
+
+;;; titlebar
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+
+;;; frame
+(add-to-list 'default-frame-alist '(height . 24))
+(add-to-list 'default-frame-alist '(width . 80))
+
+;; Package Manager
+
+;;; set sources
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;;; bootstrap
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
