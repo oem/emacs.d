40caad88e419e1c415b99d0be51a02d7ad747c14
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-18 15:25:30 +0100

    adds some more sane defaults: no backups; titlebar styling and evil mode

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; Defaults
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
 ;; UI
 ;;; deactivate ui chrome
 (scroll-bar-mode -1)
@@ -7,10 +11,12 @@
 
 ;;; typography
 (add-to-list 'default-frame-alist '(font . "Cartograph Mono CF-14"))
-(setq-default line-spacing 20)
+(setq-default line-spacing 18)
 
 ;;; titlebar
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format nil)
 
 ;;; frame
 (add-to-list 'default-frame-alist '(height . 24))
@@ -31,12 +37,27 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
+
+;; Keybindings
+;;; Evil mode setup
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+(use-package evil-escape
+  :ensure t
+  :init
+  (setq-default evil-escape-key-sequence "jj")
+  (setq-default evil-escape-delay 0.2)
+  :config
+  (evil-escape-mode 1))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (evil-escape evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
