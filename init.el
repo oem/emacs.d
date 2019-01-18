c3ab8e0982cf7eddb42bf468dd1d5dd0c6f8cf21
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-18 15:52:09 +0100

    adds some keybinds and some ui tweaks

--- init.el
+++ init.el
@@ -17,7 +17,6 @@
   (package-install 'use-package))
 (require 'use-package)
 
-
 ;; UI
 ;;; deactivate ui chrome
 (scroll-bar-mode -1)
@@ -28,7 +27,7 @@
 (setq ring-bell-function 'ignore)
 
 ;;; typography
-(add-to-list 'default-frame-alist '(font . "Cartograph Mono CF -14"))
+(add-to-list 'default-frame-alist '(font . "Cartograph Mono CF-14"))
 (setq-default line-spacing 16)
 
 ;;; titlebar
@@ -46,7 +45,22 @@
   :config
   (load-theme 'doom-one t))
 
+;;; mode line
+(use-package doom-modeline
+      :ensure t
+      :hook (after-init . doom-modeline-mode))
+(setq doom-modeline-height 35)
+
 ;; Keybindings
+;;; make keybinds discoverable with which-key
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode 1))
+
 ;;; Evil mode setup
 (use-package evil
   :ensure t
@@ -60,12 +74,41 @@
   (setq-default evil-escape-delay 0.2)
   :config
   (evil-escape-mode 1))
+
+;;; Custom keybinding with general
+(use-package general
+  :ensure t
+  :config (general-define-key
+  :states '(normal visual insert emacs)
+  :prefix "SPC"
+  :non-normal-prefix "M-SPC"
+  ;; "/"   '(counsel-rg :which-key "ripgrep") ; You'll need counsel package for this
+  "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
+  "SPC" '(helm-M-x :which-key "M-x")
+  "pf"  '(helm-find-files :which-key "find files")
+  ;; Buffers
+  "bb"  '(helm-buffers-list :which-key "buffers list")
+  ;; Window
+  "wl"  '(windmove-right :which-key "move right")
+  "wh"  '(windmove-left :which-key "move left")
+  "wk"  '(windmove-up :which-key "move up")
+  "wj"  '(windmove-down :which-key "move bottom")
+  "w/"  '(split-window-right :which-key "split right")
+  "w-"  '(split-window-below :which-key "split bottom")
+  "wx"  '(delete-window :which-key "delete window")
+  ;; Others
+  "at"  '(ansi-term :which-key "open terminal")
+))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil-escape evil use-package))))
+ '(custom-safe-themes
+   (quote
+    ("b54826e5d9978d59f9e0a169bbd4739dd927eead3ef65f56786621b53c031a7c" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" default)))
+ '(package-selected-packages (quote (which-key evil-escape evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
