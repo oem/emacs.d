bf087609735ea240dc29a2803248df0f49ab8680
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-19 17:11:53 +0100

    moves the emacs initialization into an org file

--- init.el
+++ init.el
@@ -1,130 +1,16 @@
-;; Defaults
-;;; no backups
-(setq make-backup-files nil) ; stop creating backup~ files
-(setq auto-save-default nil) ; stop creating #autosave# files
-;;; Show matching parens
-(setq show-paren-delay 0)
-(show-paren-mode 1)
+;; defining some variables
+(defvar my-init-file (expand-file-name "emacs-init.el" user-emacs-directory)
+  "All configurations stored in this file.")
 
-;; Package Manager
-;;; set sources
-(require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
-                         ("gnu"   . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
+(defvar my-org-file (expand-file-name "emacs-init.org" user-emacs-directory)
+  "All configurations tangled from this file.")
 
-;;; bootstrap
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-
-;; UI
-;;; deactivate ui chrome
-(scroll-bar-mode -1)
-(tool-bar-mode   -1)
-(tooltip-mode    -1)
-(menu-bar-mode   -1)
-;;; deactivate bells, audio or visual
-(setq ring-bell-function 'ignore)
-
-;;; typography
-(add-to-list 'default-frame-alist '(font . "Cartograph Mono CF-14"))
-(setq-default line-spacing 16)
-
-;;; titlebar
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(setq ns-use-proxy-icon nil)
-(setq frame-title-format nil)
-
-;;; frame
-(add-to-list 'default-frame-alist '(height . 24))
-(add-to-list 'default-frame-alist '(width . 80))
-
-;;; theme
-(use-package doom-themes
-  :ensure t
-  :config
-  (load-theme 'doom-Iosvkem t))
-
-;;; mode line
-(use-package doom-modeline
-      :ensure t
-      :hook (after-init . doom-modeline-mode))
-(setq doom-modeline-height 35)
-
-;; fuzzy find
-;;; Helm
-(use-package helm
-  :ensure t
-  :init
-  (setq helm-M-x-fuzzy-match t
-  helm-mode-fuzzy-match t
-  helm-buffers-fuzzy-matching t
-  helm-recentf-fuzzy-match t
-  helm-locate-fuzzy-match t
-  helm-semantic-fuzzy-match t
-  helm-imenu-fuzzy-match t
-  helm-completion-in-region-fuzzy-match t
-  helm-candidate-number-list 150
-  helm-split-window-in-side-p t
-  helm-move-to-line-cycle-in-source t
-  helm-echo-input-in-header-line t
-  helm-autoresize-max-height 0
-  helm-autoresize-min-height 20)
-  :config
-  (helm-mode 1))
-
-;; Keybindings
-;;; make keybinds discoverable with which-key
-(use-package which-key
-  :ensure t
-  :init
-  (setq which-key-separator " ")
-  (setq which-key-prefix-prefix "+")
-  :config
-  (which-key-mode 1))
-
-;;; Evil mode setup
-(use-package evil
-  :ensure t
-  :config
-  (evil-mode 1))
-
-(use-package evil-escape
-  :ensure t
-  :init
-  (setq-default evil-escape-key-sequence "jj")
-  (setq-default evil-escape-delay 0.2)
-  :config
-  (evil-escape-mode 1))
-
-;;; Custom keybinding with general
-(use-package general
-  :ensure t
-  :config (general-define-key
-  :states '(normal visual insert emacs)
-  :prefix "SPC"
-  :non-normal-prefix "M-SPC"
-  ;; "/"   '(counsel-rg :which-key "ripgrep") ; You'll need counsel package for this
-  "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
-  "SPC" '(helm-M-x :which-key "M-x")
-  "pf"  '(helm-find-files :which-key "find files")
-  ;; Buffers
-  "bb"  '(helm-buffers-list :which-key "buffers list")
-  ;; Window
-  "wl"  '(windmove-right :which-key "move right")
-  "wh"  '(windmove-left :which-key "move left")
-  "wk"  '(windmove-up :which-key "move up")
-  "wj"  '(windmove-down :which-key "move bottom")
-  "w/"  '(split-window-right :which-key "split right")
-  "w-"  '(split-window-below :which-key "split bottom")
-  "wx"  '(delete-window :which-key "delete window")
-  ;; Others
-  "at"  '(ansi-term :which-key "open terminal")
-))
+;; load the config
+(if (file-exists-p my-init-file)
+    (load-file my-init-file)
+  (progn
+    (org-babel-load-file
+     (expand-file-name "emacs-init.org" user-emacs-directory))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
