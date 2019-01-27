03609ba47edc3d40e74429b1672d64c80b8ab712
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-27 15:10:19 +0100

    adds the klere theme

---
+++ elpa/klere-theme-20180415.1823/klere-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "klere-theme" "20180415.1823" "A dark theme with lambent color highlights and incremental grays" '((emacs "24")) :commit "c064f9e5c44173c239fce239a62c8d5e61827672" :authors '(("Wamm K. D." . "jaft.r@outlook.com")) :maintainer '("Wamm K. D." . "jaft.r@outlook.com") :url "https://github.com/WammKD/emacs-klere-theme")
