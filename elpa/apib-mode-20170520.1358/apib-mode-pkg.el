7287fe44eb899326743181f9d6b7d06ba2d94882
Ömür Özkir <oemuer.oezkir@gmail.com>
2019-01-29 14:41:21 +0100

    adds apib mode

---
+++ elpa/apib-mode-20170520.1358/apib-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "apib-mode" "20170520.1358" "Major mode for API Blueprint files" '((markdown-mode "2.1")) :commit "6cc7c6f21b8e415b1718bb6a07ab2182e9e9dde6" :keywords '("tools" "api-blueprint") :authors '(("Vilibald Wanča" . "vilibald@wvi.cz")) :maintainer '("Vilibald Wanča" . "vilibald@wvi.cz") :url "http://github.com/w-vi/apib-mode")
---
+++ elpa/dash-functional-20180107.1618/dash-functional-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dash-functional" "20180107.1618" "Collection of useful combinators for Emacs Lisp" '((dash "2.0.0") (emacs "24")) :commit "63022432e1f7bcc5af9582f29d4ed0552a2e658b" :keywords '("lisp" "functions" "combinators"))
